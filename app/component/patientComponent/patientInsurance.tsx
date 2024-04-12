d7a8d91838a098de478b57add161cf01dfb4319b
Iroegbu David <iroegbu.dgit@gmail.com>
2024-04-12 15:10:56 +0100

    new commit

--- app/component/patientComponent/patientInsurance.tsx
+++ app/component/patientComponent/patientInsurance.tsx
@@ -1,9 +1,17 @@
 import { PatientTabProps } from '@/types'
 import React from 'react'
+import RouteNav from '../routeNav'
+import TabBar from './tabBar'
 
 const PatientInsurance = ({patient_tab, setPatient_tab}:PatientTabProps) => {
     return (
-        <div>PatientInsurance</div>
+        <main className="w-full h-screen flex flex-col bg-slate-100 overflow-hidden">
+            <RouteNav />
+            <TabBar patient_tab={patient_tab} setPatient_tab={setPatient_tab} />
+            <div className="w-full bg-sky-600 flex-1 py-3 overflow-hidden">
+                insuracne page
+            </div>
+        </main>
     )
 }
 
