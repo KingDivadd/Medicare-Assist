adcacde87382975bdd3403634efaac9e5de379b7
Iroegbu David <iroegbu.dgit@gmail.com>
2024-05-02 12:35:09 +0100

    new commit

--- app/component/schedulingComponent/schedulingSide.tsx
+++ app/component/schedulingComponent/schedulingSide.tsx
@@ -1,13 +1,38 @@
-'u'
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { FiRefreshCcw } from "react-icons/fi";
 import MyDatePicker from './datepicker';
 import { IoCaretUp } from "react-icons/io5";
 import { IoCaretDown } from "react-icons/io5";
 import { SelectDateProps } from '@/types';
+import DropDown from '../dropDown';
 
 
 const SchedulingSideBar = ({clickedDate, setClickedDate}: SelectDateProps) => {
+    const [dropMenus, setDropMenus] = useState<{ [key: string]: boolean }>({
+        practice: false,
+        location: false,
+        provider: false,
+    });
+    const [dropElements, setDropElements] = useState({
+        practice: 'SELECT',
+        location: 'SELECT',
+        provider: 'SELECT',
+    })
+
+    const handleSelectDropdown = (dropdown: any, title:any)=>{
+        setDropElements({...dropElements, [title]: dropdown}); setDropMenus({...dropMenus, [title]: false})
+    }
+
+
+    const handleDropMenu = (dropdown: any) => {
+        const updatedDropMenus = Object.keys(dropMenus).reduce((acc, key) => {
+            acc[key] = key === dropdown ? !dropMenus[key] : false;
+            return acc;
+        }, {} as { [key: string]: boolean });
+        setDropMenus(updatedDropMenus);
+        setDropElements({...dropElements, [dropdown]: 'SELECT'});
+    };
     return (
         <div className='w-[400px] flex items-start justify-center pl-3 bg-sky-600'>
             <div className="flex flex-col  rounded-b-[10px] w-full h-auto gap-2 pt-[15px]">
@@ -18,33 +43,25 @@ const SchedulingSideBar = ({clickedDate, setClickedDate}: SelectDateProps) => {
                 <div className=" w-full bg-red-200 pt-[1px] flex flex-col bg-white">
                     <span className="w-full flex items-center justify-center text-black text-[16px] font-semibold bg-blue-200 h-[35px]">Provider</span>
 
-                    <span className="w-full flex flex-row items-center justify-center gap-5 h-[35px] ">
+                    <span className="w-full flex flex-row items-center justify-center gap-5 py-1">
                         <p className="text-sky-600 text-[15px] font-semibold">Practice</p>
-                        <span className=" h-[23px] w-[40%] flex flex-row items-center justify-end cursor-pointer ">
-                            <span className="w-full flex h-full items-center justify-center text-slate-500 text-[14px] border-2 border-slate-400 border-r-0">HIM</span>
-                            <span className="h-full w-[40px] bg-sky-700 flex items-center justify-center">
-                                <IoCaretDown size={18} className='text-slate-200' />
-                            </span>
+                        <span className="w-[60%] flex flex-row items-center justify-end cursor-pointer ">
+                            <DropDown title={'practice'} dropArray={['HIM', 'BALDRICH']} dropElements={dropElements} dropMenus={dropMenus} handleDropMenu={handleDropMenu} handleSelectDropdown={handleSelectDropdown} setDropElements={setDropElements} setDropMenus={setDropMenus} />
                         </span>
                     </span>
-                    <span className="w-full flex flex-row items-center justify-center gap-5 h-[40px]  ">
+                    <span className="w-full flex flex-row items-center justify-center gap-5 py-1">
                         <p className="text-sky-600 text-[15px] font-semibold">Provider</p>
-                        <span className=" h-[23px] w-[40%] flex flex-row items-center justify-end cursor-pointer">
-                            <span className="w-full flex h-full items-center justify-center text-slate-500 text-[14px] font-semibold border-2 border-slate-400 border-r-0">BALDRICH</span>
-                            <span className="h-full w-[40px] bg-sky-700 flex items-center justify-center">
-                                <IoCaretDown size={18} className='text-slate-200' />
-                            </span>
+                        <span className="w-[60%] flex flex-row items-center justify-end cursor-pointer ">
+                            <DropDown title={'provider'} dropArray={['HIM', 'BALDRICH']} dropElements={dropElements} dropMenus={dropMenus} handleDropMenu={handleDropMenu} handleSelectDropdown={handleSelectDropdown} setDropElements={setDropElements} setDropMenus={setDropMenus} />
                         </span>
                     </span>
-                    <span className="w-full flex flex-row items-center justify-center gap-5 h-[40px]  ">
+                    <span className="w-full flex flex-row items-center justify-center gap-5 py-1">
                         <p className="text-sky-600 text-[15px] font-semibold">Location</p>
-                        <span className=" h-[23px] w-[40%] flex flex-row items-center justify-end cursor-pointer">
-                            <span className="w-full flex h-full items-center justify-center text-slate-500 text-[14px] font-semibold border-2 border-slate-400 border-r-0">HIM</span>
-                            <span className="h-full w-[40px] bg-sky-700 flex items-center justify-center">
-                                <IoCaretDown size={18} className='text-slate-200' />
-                            </span>
+                        <span className="w-[60%] flex flex-row items-center justify-end cursor-pointer ">
+                            <DropDown title={'location'} dropArray={['HIM', 'BALDRICH']} dropElements={dropElements} dropMenus={dropMenus} handleDropMenu={handleDropMenu} handleSelectDropdown={handleSelectDropdown} setDropElements={setDropElements} setDropMenus={setDropMenus} />
                         </span>
                     </span>
+                    
                 </div>
 
                 <div className=" w-full bg-red-200 pt-[1px] flex flex-col bg-white">
